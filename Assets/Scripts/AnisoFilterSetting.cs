3c173e740c3209fbc2738c108f139455c031091d
TheAizWall <131761448+TheAizWall@users.noreply.github.com>
2023-05-03 00:41:37 +0300

    Added Aniso
    я самий піздатий

---
+++ Assets/Scripts/AnisoFilterSetting.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnisoFilterSetting : MonoBehaviour
+{
+    [SerializeField] private Dropdown _anisoFilterDropdown;
+
+    private void SetAnisoFilter(int index)
+    {
+        switch(index)
+        {
+            case 0:
+                QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
+                break;
+            case 1:
+                QualitySettings.anisotropicFiltering = AnisotropicFiltering.Enable;
+                break;
+            case 2:
+                QualitySettings.anisotropicFiltering = AnisotropicFiltering.ForceEnable;
+                break;
+            default:
+                break;
+        }
+
+    }
+}
