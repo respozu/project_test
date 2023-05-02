77b2ddf779cc6a14d4bb22ea9527a2dc1e85373e
respozu <vsemobile58@gmail.com>
2023-05-03 01:43:33 +0300

    added racket kicks

--- Assets/Scripts/TennisPlayer.cs
+++ Assets/Scripts/TennisPlayer.cs
@@ -8,15 +8,19 @@ public class TennisPlayer : MonoBehaviour
 
     [SerializeField] private float rotationSpeed;
 
+    private bool _isKicked;
+
     private PlayerInput _input;
 
     private float _startXPos;
     private float _startZRot;
 
-    private void Start()
+    private void Awake()
     {
         _input = new PlayerInput();
-        _input.Player.Click.performed += context => Kick();
+
+        _input.Player.Click.performed += context => StartKickCoroutine();
+
         _startXPos = transform.position.x;
         _startZRot = transform.eulerAngles.z;
     }
@@ -28,9 +32,9 @@ public class TennisPlayer : MonoBehaviour
 
     private void Update()
     {
-        Vector3 racketNewPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, 1));
-        transform.position = racketNewPosition;
-        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, racketNewPosition.z * rotationSpeed + _startZRot);
+        TransformRacket();
+
+        
     }
 
     private void OnDisable()
@@ -38,9 +42,45 @@ public class TennisPlayer : MonoBehaviour
         _input.Disable();
     }
 
+    private void TransformRacket()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+
+        Vector3 newPositionWithoutX = new Vector3(transform.position.x, newPosition.y, newPosition.z);
+
+        transform.position = newPositionWithoutX;
+
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, newPosition.z * rotationSpeed + _startZRot);
+    }
+
+
+
+    private void StartKickCoroutine()
+    {
+        _isKicked = true;
+        StartCoroutine(Kick());
+    }
+
     private IEnumerator Kick()
     {
-        Debug.Log("trapped");
+        Vector3 pos = transform.position;
+        float goingX = pos.x - 1;
+        while (_isKicked)
+        {
+            pos.x = Mathf.Lerp(pos.x, goingX, 0.2f);
+            transform.position = pos;
+            if (Mathf.Abs(transform.position.x - goingX) <= 0.1f) _isKicked = false; 
+            yield return new WaitForFixedUpdate();
+        }
+        bool kickEnds = true;
+        float returningX = goingX + 1;
+        while (kickEnds)
+        {
+            pos.x = Mathf.Lerp(pos.x, returningX, 0.2f);
+            transform.position = pos;
+            if (Mathf.Abs(transform.position.x - returningX) <= 0.1f) kickEnds = false;
+            yield return new WaitForFixedUpdate();
+        }
         yield return null;
     }
 }
