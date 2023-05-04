19777bef8989dffa43df34c3815d07ada3916189
TheAizWall <131761448+TheAizWall@users.noreply.github.com>
2023-05-04 14:34:41 +0300

    fix

--- Assets/Scripts/RecordCounter.cs
+++ Assets/Scripts/RecordCounter.cs
@@ -6,24 +6,26 @@ public class RecordCounter : MonoBehaviour
     private int _score;
     public int Score => _score;
 
-    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI scoreText;
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("TennisBall"))
         {
             _score++;
+            UpdateScoreText();
         }
     }
 
     public void ResetScore()
     {
         _score = 0;
+        UpdateScoreText();
     }
 
     private void UpdateScoreText()
     {
-        _scoreText.text = $"Score: {_score}";
+        scoreText.text = $"Score: {_score}";
     }
 }
 
