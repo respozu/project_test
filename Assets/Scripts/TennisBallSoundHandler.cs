8978c07984c532b34c5acfd00a3d5e0ea9dcb144
TheAizWall <131761448+TheAizWall@users.noreply.github.com>
2023-05-09 23:12:47 +0300

    addon

--- Assets/Scripts/TennisBallSoundHandler.cs
+++ Assets/Scripts/TennisBallSoundHandler.cs
@@ -1,43 +1,43 @@
-using System.Collections;
-using UnityEngine;
+//using System.Collections;
+//using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
-public class TennisBallSoundHandler : MonoBehaviour
-{
-    [SerializeField] private AudioClip defaulthitSound;
-    [SerializeField] private AudioClip racketHitSound;
+//[RequireComponent(typeof(AudioSource))]
+//public class TennisBallSoundHandler : MonoBehaviour
+//{
+//    [SerializeField] private AudioClip defaulthitSound;
+//    [SerializeField] private AudioClip racketHitSound;
 
-    private AudioSource _audioSource;
-    private bool _canPlaySound = true;
+//    private AudioSource _audioSource;
+//    private bool _canPlaySound = true;
 
-    private void Start()
-    {
-        _audioSource = GetComponent<AudioSource>();
-    }
+//    private void Start()
+//    {
+//        _audioSource = GetComponent<AudioSource>();
+//    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (_canPlaySound)
-        {
-            float pitch = Random.Range(0.9f, 1.1f);
-            _audioSource.pitch = pitch;
-            if (collision.gameObject.)
-            {
-                _audioSource.PlayOneShot(racketHitSound);
-            }
-            else
-            {
-                _audioSource.Play();
-                _canPlaySound = false;
-                StartCoroutine(KickSoundCooldown());
-            }
-        }
+//    private void OnCollisionEnter(Collision collision)
+//    {
+//        if (_canPlaySound)
+//        {
+//            float pitch = Random.Range(0.9f, 1.1f);
+//            _audioSource.pitch = pitch;
+//            //if (collision.gameObject.)  
+//            {
+//                _audioSource.PlayOneShot(racketHitSound);
+//            }
+//            else
+//            {
+//                _audioSource.Play();
+//                _canPlaySound = false;
+//                StartCoroutine(KickSoundCooldown());
+//            }
+//        }
 
-    }
+//    }
 
-    IEnumerator KickSoundCooldown()
-    {
-        yield return new WaitForSeconds(0.15f);
-        _canPlaySound = true;
-    }
-}
+//    IEnumerator KickSoundCooldown()
+//    {
+//        yield return new WaitForSeconds(0.15f);
+//        _canPlaySound = true;
+//    }
+//}
