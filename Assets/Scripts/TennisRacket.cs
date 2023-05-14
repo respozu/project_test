6309778c87b070131126bb39cf9ed21f8539b71f
respozu <vsemobile58@gmail.com>
2023-05-14 14:14:21 +0300

    1

--- Assets/Scripts/TennisRacket.cs
+++ Assets/Scripts/TennisRacket.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -13,6 +14,7 @@ public class TennisRacket : MonoBehaviour
     private Vector2 _yRotationBorders;
     private Vector2 _zRotationBorders;
 
+    private Vector3 _startPos;
     private Vector3 _startRot;
     
     private Rigidbody _rb;
@@ -20,7 +22,8 @@ public class TennisRacket : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        
+
+        _startPos = transform.position;
         _startRot = transform.eulerAngles;
 
         _xRotationBorders.x = _startRot.x - xRotationRange / 2;
@@ -36,20 +39,15 @@ public class TennisRacket : MonoBehaviour
     public void Move(Vector3 newPos)
     {
         Quaternion newRotation = Quaternion.Euler(
-            Mathf.Clamp(_startRot.x + -newPos.z * rotationSpeed.x,_xRotationBorders.x, _xRotationBorders.y),
+            Mathf.Clamp(_startRot.x + -newPos.y * rotationSpeed.x,_xRotationBorders.x, _xRotationBorders.y),
             Mathf.Clamp(_startRot.y + -newPos.z * rotationSpeed.y,_yRotationBorders.x, _yRotationBorders.y),  
-            Mathf.Clamp(_startRot.z + newPos.y * rotationSpeed.z, _zRotationBorders.x, _zRotationBorders.y));
-        
-        Quaternion newRotation1 = Quaternion.Euler(
-            Mathf.Clamp(_startRot.x + -newPos.z * rotationSpeed.x,_xRotationBorders.x, _xRotationBorders.y),
-            Mathf.Clamp(_startRot.y + -newPos.z * rotationSpeed.y,_yRotationBorders.x, _yRotationBorders.y),  
-            Mathf.Clamp(_startRot.z + newPos.y * rotationSpeed.z, _zRotationBorders.x, _zRotationBorders.y));
-            );
+            Mathf.Clamp(_startRot.z + newPos.z * rotationSpeed.z, _zRotationBorders.x, _zRotationBorders.y));
+            
 
-        //if ()
-        {
-        //    newRotation = Quaternion.Inverse(newRotation);
-        }
+        // if (_startPos.x < 0)
+        // {
+        //     newRotation = Quaternion.
+        // }
 
         _rb.Move(newPos, newRotation);
     }
