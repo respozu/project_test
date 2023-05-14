311c3c086eca62c0e28871cf0b518af12d409e23
respozu <vsemobile58@gmail.com>
2023-05-14 16:19:41 +0300

    added mirror

--- Assets/Scripts/TennisRacket.cs
+++ Assets/Scripts/TennisRacket.cs
@@ -1,5 +1,5 @@
-using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.Networking;
 
 [RequireComponent(typeof(Rigidbody))]
 public class TennisRacket : MonoBehaviour
@@ -23,8 +23,8 @@ public class TennisRacket : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody>();
 
-        _startPos = transform.position;
-        _startRot = transform.eulerAngles;
+        _startPos = transform.localPosition;
+        _startRot = transform.localEulerAngles;
 
         _xRotationBorders.x = _startRot.x - xRotationRange / 2;
         _xRotationBorders.y = _startRot.x + xRotationRange / 2;
@@ -38,16 +38,22 @@ public class TennisRacket : MonoBehaviour
 
     public void Move(Vector3 newPos)
     {
+        float newRotY = _startRot.y - newPos.z * rotationSpeed.y;
+
+        if (_startPos.x < 0)
+        {
+            newRotY = _startRot.y + newPos.z * rotationSpeed.y;
+        }
+        
         Quaternion newRotation = Quaternion.Euler(
             Mathf.Clamp(_startRot.x + -newPos.y * rotationSpeed.x,_xRotationBorders.x, _xRotationBorders.y),
-            Mathf.Clamp(_startRot.y + -newPos.z * rotationSpeed.y,_yRotationBorders.x, _yRotationBorders.y),  
+            Mathf.Clamp(newRotY,_yRotationBorders.x, _yRotationBorders.y),  
             Mathf.Clamp(_startRot.z + newPos.z * rotationSpeed.z, _zRotationBorders.x, _zRotationBorders.y));
-            
 
-        // if (_startPos.x < 0)
-        // {
-        //     newRotation = Quaternion.
-        // }
+        if (_startPos.x < 0)
+        {
+            
+        }
 
         _rb.Move(newPos, newRotation);
     }
